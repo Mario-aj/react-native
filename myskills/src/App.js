1f50b4d9f924f94dfe783369621880130994a73f
mario-aj <mariojorge1997@mail.ru>
2022-07-08 15:14:14 +0100

    extract code to new page and use StatusBar component

--- myskills/src/App.js
+++ myskills/src/App.js
@@ -1,69 +1,13 @@
 import React from 'react';
-import {
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  Platform,
-} from 'react-native';
+import { StatusBar } from 'react-native';
 
-import { Button } from './components/Button';
-import { CardSkill } from './components/CardSkill';
+import { Home } from './pages/home';
 
 export default function App() {
-  const [newSkill, setNewSkill] = React.useState('');
-  const [skills, setSkills] = React.useState([]);
-
-  const handleAddSkill = () => {
-    if (newSkill.trim() === '') {
-      return;
-    }
-
-    setSkills(oldSkills => [...oldSkills, newSkill]);
-    setNewSkill('');
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Welcome, Mário</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="New skill"
-        placeholderTextColor="#666"
-        onChangeText={setNewSkill}
-        value={newSkill}
-      />
-      <Button onPress={handleAddSkill} />
-      <Text style={[styles.title, styles.mySkills]}>My Skills</Text>
-      {skills.map(skill => (
-        <CardSkill key={skill} skill={skill} />
-      ))}
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#121015" />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121015',
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    backgroundColor: '#1f1e25',
-    color: '#fff',
-    borderRadius: 4,
-    fontSize: 18,
-    padding: Platform.OS === 'android' ? 10 : 15,
-    marginBottom: 15,
-  },
-  mySkills: {
-    marginVertical: 40,
-  },
-});
---
+++ myskills/src/pages/home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
+
+import { Button } from '../components/Button';
+import { CardSkill } from '../components/CardSkill';
+
+export function Home() {
+  const [newSkill, setNewSkill] = React.useState('');
+  const [skills, setSkills] = React.useState([]);
+
+  const handleAddSkill = () => {
+    if (newSkill.trim() === '') {
+      return;
+    }
+
+    setSkills(oldSkills => [...oldSkills, newSkill]);
+    setNewSkill('');
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome, Mário</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#666"
+        onChangeText={setNewSkill}
+        value={newSkill}
+      />
+      <Button onPress={handleAddSkill} />
+      <Text style={[styles.title, styles.mySkills]}>My Skills</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={skills}
+        renderItem={({ item }) => <CardSkill skill={item} />}
+        keyExtractor={item => item}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    borderRadius: 4,
+    fontSize: 18,
+    padding: Platform.OS === 'android' ? 10 : 15,
+    marginBottom: 15,
+  },
+  mySkills: {
+    marginVertical: 40,
+  },
+});
