fd0f7e002ef2492e8ae7b6b90e4b52995e739952
mario-aj <mariojorge1997@mail.ru>
2022-07-09 13:21:28 +0100

    add taskList component

--- todo/src/components/addTak.tsx
+++ todo/src/components/addTak.tsx
@@ -9,21 +9,21 @@ import {
 
 export const AddTask = () => {
   return (
-    <View style={styles.addTask}>
+    <View style={styles.container}>
       <TextInput
-        style={styles.addTaskInput}
+        style={styles.input}
         placeholder="Add a task"
         placeholderTextColor="#B2B2B2"
       />
-      <TouchableOpacity style={styles.addTaskButton} activeOpacity={0.5}>
-        <Text style={styles.addTaskText}>+</Text>
+      <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+        <Text style={styles.buttonText}>+</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  addTask: {
+  container: {
     flexDirection: 'row',
     alignItems: 'center',
     height: 56,
@@ -32,14 +32,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#e6e6f0',
     marginTop: -32,
+    marginBottom: 32,
+    marginHorizontal: 24,
   },
-  addTaskInput: {
+  input: {
     flex: 1,
     height: '100%',
     paddingHorizontal: 16,
     fontSize: 16,
   },
-  addTaskButton: {
+  button: {
     width: 56,
     height: '100%',
     justifyContent: 'center',
@@ -47,7 +49,7 @@ const styles = StyleSheet.create({
     borderLeftWidth: 1,
     borderLeftColor: '#e6e6f0',
   },
-  addTaskText: {
+  buttonText: {
     fontSize: 32,
     color: '#8257e5',
   },
--- todo/src/pages/home.tsx
+++ todo/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
 import { AddTask } from '../components/addTak';
 import { Header } from '../components/header';
 
@@ -9,6 +10,36 @@ export const Home = () => {
       <Header />
       <View style={styles.body}>
         <AddTask />
+        <FlatList
+          data={[
+            'Ler meu livro favorito',
+            'Arrumar o quarto',
+            'Lavar meu possante',
+            'Estudar react',
+            'Ler meu livro favorito',
+            'Arrumar o quarto',
+            'Lavar meu possante',
+            'Estudar react',
+            'Ler meu livro favorito',
+            'Arrumar o quarto',
+            'Lavar meu possante',
+            'Estudar react',
+            'Ler meu livro favorito',
+            'Arrumar o quarto',
+            'Lavar meu possante',
+            'Estudar react',
+            'Ler meu livro favorito',
+            'Arrumar o quarto',
+            'Lavar meu possante',
+            'Estudar react',
+          ]}
+          keyExtractor={(item, idx) => item + idx}
+          renderItem={({ item, index }) => (
+            <View style={[styles.cardTaks, index % 2 === 0 && styles.evenCard]}>
+              <Text style={styles.carItem}>{item}</Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -20,6 +51,18 @@ const styles = StyleSheet.create({
   },
   body: {
     flex: 1,
-    paddingHorizontal: 24,
+  },
+  cardTaks: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  evenCard: {
+    backgroundColor: '#ddd',
+  },
+  carItem: {
+    fontSize: 14,
+    color: '#666',
   },
 });
