36ff55fd74fa9f5309e4fab855d6b0438f0c7596
mario-aj <mariojorge1997@mail.ru>
2022-07-09 12:47:11 +0100

    addtask component

--- todo/src/pages/home.tsx
+++ todo/src/pages/home.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export const Home = () => {
   return (
@@ -8,12 +14,24 @@ export const Home = () => {
         <View style={styles.headerContent}>
           <Text style={styles.logo}>to.do</Text>
           <View style={styles.teskCounter}>
-            <Text style={styles.headerText}>Você tem</Text>
+            <Text style={styles.headerText}>You have</Text>
             <Text style={[styles.headerText, styles.headerTextBold]}>
               {' '}
-              3 tarefas
+              3 tasks
             </Text>
           </View>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.addTask}>
+          <TextInput
+            style={styles.addTaskInput}
+            placeholder="Add a task"
+            placeholderTextColor="#B2B2B2"
+          />
+          <TouchableOpacity style={styles.addTaskButton} activeOpacity={0.5}>
+            <Text style={styles.addTaskText}>+</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -27,7 +45,7 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     alignItems: 'flex-start',
-    height: 150,
+    height: 140,
     width: '100%',
     backgroundColor: '#8257E5',
     paddingHorizontal: 24,
@@ -55,5 +73,37 @@ const styles = StyleSheet.create({
   },
   headerTextBold: {
     fontWeight: 'bold',
+  },
+  body: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  addTask: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 56,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#e6e6f0',
+    marginTop: -32,
+  },
+  addTaskInput: {
+    flex: 1,
+    height: '100%',
+    paddingHorizontal: 16,
+    fontSize: 16,
+  },
+  addTaskButton: {
+    width: 56,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderLeftColor: '#e6e6f0',
+  },
+  addTaskText: {
+    fontSize: 32,
+    color: '#8257e5',
   },
 });
