78803d198ea78b35ecad60e213f9d7ac9a0f5ec9
mario-aj <mariojorge1997@mail.ru>
2022-07-10 19:53:02 +0100

    creating our theme and provide it to all components

--- gofinances/src/index.tsx
+++ gofinances/src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { StatusBar, SafeAreaView } from "react-native";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./global/theme";
 import { Dashboard } from "./Dashboard";
 
 export function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Dashboard />
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <Dashboard />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
