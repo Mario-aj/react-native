28d54e5206cef1088f096183065e08ef55c760ba
mario-aj <mariojorge1997@mail.ru>
2022-07-10 20:17:03 +0100

    declaring our theme types into styled default theme

--- gofinances/src/Dashboard/styles.ts
+++ gofinances/src/Dashboard/styles.ts
@@ -1,13 +1,19 @@
 import styled from "styled-components/native";
+import { css } from "styled-components";
 
-export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
+export const Container = styled.View(
+  ({ theme }) => css`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.background};
+  `
+);
 
-export const Title = styled.Text`
-  font-size: 24px;
-  color: #f00;
-  font-weight: bold;
-`;
+export const Title = styled.Text(
+  ({ theme }) => css`
+    font-size: 24px;
+    color: ${theme.colors.title};
+    font-weight: bold;
+  `
+);
