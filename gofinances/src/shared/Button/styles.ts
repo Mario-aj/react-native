b08ccab6360820f4b6aebd6b38f058626502fb3f
mario-aj <mariojorge1997@mail.ru>
2022-07-18 18:58:52 +0100

    style send form button

--- gofinances/src/shared/Button/styles.ts
+++ gofinances/src/shared/Button/styles.ts
@@ -1,6 +1,24 @@
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled(TouchableOpacity)``;
+export const Container = styled(TouchableOpacity)(
+  ({ theme }) => css`
+    background-color: ${theme.colors.secondary};
+    width: 100%;
+    align-items: center;
 
-export const Title = styled.Text``;
+    border-radius: 6px;
+
+    padding: 18px;
+    margin-top: auto;
+  `
+);
+
+export const Title = styled.Text(
+  ({ theme }) => css`
+    font-family: ${theme.fonts.medium};
+    font-size: ${RFValue(14)}px;
+    color: ${theme.colors.shape};
+  `
+);
