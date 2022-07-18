ce2983edfd50c151be6ca3f4556a29a62df11f90
mario-aj <mariojorge1997@mail.ru>
2022-07-18 18:52:29 +0100

    Button component

---
+++ gofinances/src/shared/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, Title } from "./styles";
+
+type Props = TouchableOpacityProps & {
+  title: string;
+};
+
+export const Button = ({ title, ...props }: Props) => {
+  return (
+    <Container {...props}>
+      <Title>{title}</Title>
+    </Container>
+  );
+};
